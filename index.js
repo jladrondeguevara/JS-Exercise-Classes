f73cfa9a2bd021df2dd3818a4a99cd115498b153
Jeovanni <jeovannildg@gmail.com>
2020-04-28 21:11:46 -0600

    completed car class

--- index.js
+++ index.js
@@ -78,7 +78,28 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon)
+  {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance){
+    const potentialDistance = this.tank * this.milesPerGallon;
+    if(distance <= potentialDistance)
+    {
+      this.odometer += distance;
+      this.tank -= (distance/this.milesPerGallon);
+    } else {
+      this.odometer = potentialDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
 
 /*
